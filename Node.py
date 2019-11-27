8d00d38367f5068fef9e28733744419aa067653a
Shiva <shivasadat.mahdavian@divar.ir>
2019-11-27 22:56:40 +0330

    Remove errors

--- Node.py
+++ Node.py
@@ -1,4 +1,5 @@
 import datetime
+import pickle
 import socket
 import time
 from threading import Thread
@@ -16,12 +17,12 @@ class HelloThread(Thread):
     def run(self):
         while True:
             msg_list = ["random/file", "file1", "file2"]
+            encoded_msg = [item.encode() for item in msg_list]
+            data = pickle.dumps(msg_list)
             sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-            sock.bind(self._ip, self._port)
-            sock.sendto(bytes(msg_list))
-            print(datetime.datetime.now())
-            time.sleep(secs=HelloThread.hello_time)
-            print(datetime.datetime.now())
+            # sock.bind((self._ip, self._port))
+            sock.sendto(data, (self._ip, self._port))
+            time.sleep(HelloThread.hello_time)
             if self.exit:
                 break
 
@@ -33,8 +34,7 @@ class Node:
         self._hello_thread = HelloThread(self._server_ip, self._hello_port)
 
     def start(self):
-        # self._hello_thread.start()
-        pass
+        self._hello_thread.start()
 
     def cmdline(self):
         while True:
@@ -47,12 +47,14 @@ class Node:
                 self._hello_thread.exit = True
                 break
             else:
-                print("Not a valid cmd")
+                print("Not a valid command")
+
+    def connect(self, ip, port):
+        pass
 
 
 SERVER_IP = "127.0.0.1"
 SERVER_HELLO_PORT = 4000
-
 
 node = Node()
 node.start()
